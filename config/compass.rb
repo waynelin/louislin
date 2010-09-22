5975a4361e4cf5dc1e05df1f972309de3a56a610
Wayne Lin <waynelin612@gmail.com>
2010-09-22 06:53:35 -0400

    coming soon splash screen only

---
+++ config/compass.rb
@@ -0,0 +1,16 @@
+# This configuration file works with both the Compass command line tool and within Rails.
+# Require any additional compass plugins here.
+project_type = :rails
+project_path = Compass::AppIntegration::Rails.root
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "static/stylesheets"
+http_stylesheets_path = "/stylesheets"
+sass_dir = "views/stylesheets"
+images_dir = "static/images"
+http_images_path = "/images"
+javascripts_dir = "javascripts"
+environment = Compass::AppIntegration::Rails.env
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = true
--- site.rb
+++ site.rb
@@ -1,11 +1,21 @@
 require 'rubygems'
 require 'sinatra'
-
+require 'sass'
 require 'compass'
 require 'haml'
 
-configure do 
-  Compass.configuration.parse(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+# configure do 
+#  Compass.configuration.parse(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+# end
+
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views'
+  end
+  
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
 end
 
 get '/' do
@@ -16,6 +26,12 @@ get '/:page' do
   haml params[:page].to_sym
 end
 
+=begin
+get '/screen.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :screen
+end
+=end
 
 get '/stylesheets/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
